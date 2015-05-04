c245a9e236f6314a4f2c5fc762a2f963c66bb7a8
Dinis Cruz <dinis.cruz@owasp.org>
2015-05-04 13:44:00 +0100

    trying to open app.js from tm-design

--- app.js
+++ app.js
@@ -1,13 +1,9 @@
 console.log('>>>>> this is in app.js')
 
-var http = require('http');
 var port = process.env.port || 1337;
 
-console.log('>>>>> starting test server in azure in port ' + port)
+console.log('>>>>> port is set to: ' + port)
 
-http.createServer(function(req, res){
-    res.writeHead(200, {'Content-Type':'text/plain'});
-    res.end('Hello World\n');
-}).listen(port);
+require('../git_repos/TM_4_0_Windows/tm-design/app.js')
 
 console.log('>>>>> done')
