4ee2600512702246f479ef293856d1d6fadb186e
Dinis Cruz <dinis.cruz@owasp.org>
2015-05-04 20:08:51 +0100

    fixing require path

--- app.js
+++ app.js
@@ -7,6 +7,6 @@ console.log('>>>>> port is set to: ' + port)
 require('../git_repos/TM_4_0_Design/app.js')
 
 process.env.PORT=1332
-require('../git_repos/TM_4_0_GraphDB/tm-graphdb/index.js')
+require('../git_repos/TM_4_0_GraphDB/index.js')
 
 console.log('>>>>> done')
