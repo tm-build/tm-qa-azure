cd207f98d8c355e2c0adbfe944a02564258d2618
Dinis Cruz <dinis.cruz@owasp.org>
2015-05-04 20:07:14 +0100

    removing copy

--- app.js
+++ app.js
@@ -4,9 +4,9 @@ var port = process.env.port || 1337;
 
 console.log('>>>>> port is set to: ' + port)
 
-require('../git_repos/TM_4_0_Windows/tm-design/app.js')
+require('../git_repos/TM_4_0_Design/app.js')
 
 process.env.PORT=1332
-require('../git_repos/TM_4_0_Windows/tm-graphdb/index.js')
+require('../git_repos/TM_4_0_GraphDB/tm-graphdb/index.js')
 
 console.log('>>>>> done')
